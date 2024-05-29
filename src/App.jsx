f9eb08d3692ec2f52a8acfc4be1bd120e900ec0f
somyakant9 <msomya143@gmail.com>
2024-05-29 15:39:22 +0530

    added router and layout with fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu,{loader as menuLoader} from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader:menuLoader,
+        errorElement: <Error/>
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <div>Hello Vite</div>      
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,18 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+import { formatCurrency } from "../../utils/helpers";
+
+// eslint-disable-next-line react/prop-types
+function MenuItem( {pizza} ) {
+  // console.log(pizza);
+  const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients?.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>

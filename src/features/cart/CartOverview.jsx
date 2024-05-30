aa87e56db1059fbaa26ed924398de319e391dacd
somyakant9 <msomya143@gmail.com>
2024-05-30 22:17:17 +0530

    tailwind basic css applied

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 text-stone-200 uppercase px-4 py-4 sm:px-6">
+      <p className="font-semibold text-stone-300 space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to='/cart'>Open cart &rarr;</Link>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,16 @@
 // import React from 'react'
 
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 export default function Header() {
   return (
-    <header>
-        <Link to='/'>Fast React Pizza Co.</Link>
-        <SearchOrder/>
-        <p>Sam</p>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 ">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <p className="text-sm font-semibold">Sam</p>
     </header>
-  )
+  );
 }
-
